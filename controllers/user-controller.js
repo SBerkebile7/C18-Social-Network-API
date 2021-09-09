0fffb36efcb791c8819a58e80ad1eac01454310e
Spenny Berks <sberkebile@live.com>
2021-09-09 15:28:58 -0400

    finalized routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,33 +1,25 @@
 const { Thought, User } = require('../models');
 
 const thoughtController = {
-    addComment({ params, body }, res) {
-        console.log(params);
-        Comment.create(body)
-        .then(({ _id }) => {
-            return Thought.findOneAndUpdate(
-            { _id: params.thoughtId },
-            { $push: { comments: _id } },
-            { new: true }
-            );
-        })
-        .then(dbPizzaData => {
-            console.log(dbPizzaData);
-            if (!dbPizzaData) {
-            res.status(404).json({ message: 'No pizza found with this id!' });
-            return;
-            }
-            res.json(dbPizzaData);
-        })
-        .catch(err => res.json(err));
-    },
-    addReaction() {
-        
-    },
-    removeComment() {
+    getAllUsers() {
 
     },
-    removeReply() {
+    getUserById() {
+        
+    },
+    createUser() {
+
+    },
+    updateUser() {
+
+    },
+    deleteUser() {
+
+    },
+    addFriend() {
+
+    },
+    removeFriend() {
 
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,3 @@ router.use((req, res) => {
 });
 
 module.exports = router;
-const router = require('express').Router();
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-    res.status(404).send('<h1>😝 404 Error!</h1>');
-});
-
-module.exports = router;
