118dc57064b1466da6d3109992f83a57fa6b758a
Spenny Berks <sberkebile@live.com>
2021-09-09 15:39:42 -0400

    work done to user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,25 +1,64 @@
 const { Thought, User } = require('../models');
 
 const thoughtController = {
-    getAllUsers() {
+    getAllUsers(req, res) {
+        User.find({})
+        .select('__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+    getUserById({ params }, res) {
+        User.findOne({ _id: params._id })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .select('__v')
+        .then(dbUserData => {
+            console.log(dbUserData);
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    },
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    },
+    addFriend({ params }, res) {
 
     },
-    getUserById() {
-        
-    },
-    createUser() {
-
-    },
-    updateUser() {
-
-    },
-    deleteUser() {
-
-    },
-    addFriend() {
-
-    },
-    removeFriend() {
+    removeFriend({ params }, res) {
 
     }
 }
